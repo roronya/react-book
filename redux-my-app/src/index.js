b19dc024c34e697592f2e180e4e314e26abbb9f6
roronya <roronya628@gmail.com>
2018-05-14 10:24:16 +0900

    create redux-app

---
+++ redux-my-app/src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux';
+
+const initialState = {
+	tasks: []
+};
+
+function tasksReducer(state = initialState, action) {
+	switch (action.type) {
+		case 'ADD_TASK':
+			return {
+				...state,
+				tasks: state.tasks.concat([action.task])
+			};
+		default:
+			return state;
+	}
+}
+
+const store = createStore(tasksReducer);
+
+const addTask = (task) => ({
+	type: 'ADD_TASK',
+	payload: {
+		task
+	}
+});
+
+store.dispatch(addTask('Storeを学ぶ'));
+
+console.log(store.getState());
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
