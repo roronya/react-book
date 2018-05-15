8725b08ed69bad1d330a70c1e6c3a6fc56f7a0b4
roronya <roronya628@gmail.com>
2018-05-16 07:52:20 +0900

    ファイル分割

--- redux-my-app/src/index.js
+++ redux-my-app/src/index.js
@@ -1,63 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
+import tasksReducer from './reducers/tasks'
+import TodoApp from './components/TodoApp'
 import {createStore} from 'redux';
 
-const initialState = {
-	task: '',
-	tasks: []
-};
-
-function tasksReducer(state = initialState, action) {
-	switch (action.type) {
-		case 'INPUT_TASK':
-			return {
-				...state,
-				task: action.payload.task
-			};
-		case 'ADD_TASK':
-			return {
-				...state,
-				tasks: state.tasks.concat([action.payload.task])
-			};
-		default:
-			return state;
-	}
-}
-
 const store = createStore(tasksReducer);
-
-const inputTask = (task) => ({
-	type: 'INPUT_TASK',
-	payload: {
-		task
-	}
-});
-
-const addTask = (task) => ({
-	type: 'ADD_TASK',
-	payload: {
-		task
-	}
-});
-
-function TodoApp({ store }) {
-	const {task, tasks } = store.getState();
-	return (
-		<div>
-			<input type="text" onChange={(e) => store.dispatch(inputTask(e.target.value))} />
-			<input type="button" value="add" onClick={() => store.dispatch(addTask(task))} />
-			<ul>
-				{
-					tasks.map(function(item, i) {
-						return (
-							<li key={i}>{item}</li>
-						);
-					})
-				}
-			</ul>
-		</div>
-	)
-}
 
 function renderApp(store) {
 	render(
