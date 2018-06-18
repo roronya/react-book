0dbcf846ec19accce12aa5f5556b9d726a1058b2
roronya <roronya628@gmail.com>
2018-06-18 12:08:16 +0900

    create-react-app

--- yahoo-shopping-ranking/src/App.js
+++ yahoo-shopping-ranking/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
-import Ranking from './components/Ranking';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ul>
-          <li><Link to="/all">すべてのカテゴリ</Link></li>
-					<li><Link to="/category/2502">パソコン、周辺機器</Link></li>
-					<li><Link to="/category/10002">本、雑誌、コミック</Link></li>
-				</ul>
-				<Route path="/all" component={Ranking} />
-				<Route path="/category/:id" render={ ({ match }) => <Ranking categoryId={match.params.id} /> } />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
--- yahoo-shopping-ranking/src/index.js
+++ yahoo-shopping-ranking/src/index.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
-import createBrowserHistory from 'history/createBrowserHistory';
+import './index.css';
 import App from './App';
-import createStore from './createStore';
+import registerServiceWorker from './registerServiceWorker';
 
-const history = createBrowserHistory();
-
-const store = createStore(history);
-
-ReactDOM.render(
-	<Provider store={store}>
-		<ConnectedRouter history={history}>
-			<App />
-		</ConnectedRouter>
-	</Provider>,
-	document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
