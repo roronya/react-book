f77f9ac5118b8142bd5926e85366911a67d3d61a
roronya <roronya628@gmail.com>
2018-06-18 17:57:05 +0900

    Nav

--- yahoo-shopping-ranking/src/App.js
+++ yahoo-shopping-ranking/src/App.js
@@ -1,17 +1,13 @@
 import React, {Component} from 'react';
 import {Route, Link} from 'react-router-dom';
 import Ranking from './containers/Ranking';
+import Nav from './containers/Nav';
 
 class App extends Component {
     render() {
         return (
             <div className='App'>
-                <ul>
-                    <li><Link to='/all'>すべてのカテゴリ</Link></li>
-                    <li><Link to='/category/2502'>パソコン、周辺機器</Link></li>
-                    <li><Link to='/category/10002'>本、雑誌、コミック</Link></li>
-                </ul>
-
+                <Nav />
                 <Route
                     path='/category/:id'
                     render={
