2b8493ce464302895b70be55d4b861cf7c0221a8
roronya <roronya628@gmail.com>
2018-06-19 09:18:06 +0900

    title bar

--- yahoo-shopping-ranking/src/App.js
+++ yahoo-shopping-ranking/src/App.js
@@ -2,11 +2,24 @@ import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
 import Ranking from './containers/Ranking';
 import Nav from './containers/Nav';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 class App extends Component {
     render() {
         return (
             <div className='App'>
+                <CssBaseline/>
+                <AppBar>
+                    <Toolbar>
+                        <Typography type='title' color='inherit'>
+                            Yahoo!ショッピングランキング
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+
                 <Nav/>
                 <Route
                     path='/category/:id'
